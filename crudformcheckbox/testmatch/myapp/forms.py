bef2cf14d7edcfeb0d5496bc44c60872737fb44b
balaji <balajik1924@gmail.com>
2025-07-01 14:15:50 +0530

    Update forms.py

--- crudformcheckbox/testmatch/myapp/forms.py
+++ crudformcheckbox/testmatch/myapp/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from myapp.models import Checkbox
 
 class CheckboxForm(forms.ModelForm):
+	name=forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+	language=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+	gender=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 	class Meta:
 		model = Checkbox
 		fields = ['name','language','gender']
